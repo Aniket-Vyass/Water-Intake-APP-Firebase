58217a50625418896319929a009ea2f530dd350e
Aniket-Vyass <aniketvyas05@gmail.com>
2026-02-01 00:01:05 +0530

    Add fetchWaterData method to retrieve water data from Firebase

--- lib/models/water_model.dart
+++ lib/models/water_model.dart
@@ -1,3 +1,7 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
 class WaterModel {
   final String? id;
   final double amount;
@@ -23,5 +27,27 @@ class WaterModel {
   //convert watermodel to json for sending data to firebase
   Map<String, dynamic> toJson() {
     return {'amount': amount, 'dateTime': DateTime.now()};
+  }
+
+  Future<List<WaterModel>> fetchWaterData() async {
+    //fetch water data from firebase
+    final url = Uri.https(
+      'water-intake-app-e76bf-default-rtdb.asia-southeast1.firebasedatabase.app',
+      'water.json',
+    );
+    var response = await http.get(url);
+    final Map<String, dynamic> extractedData = json.decode(response.body);
+    final List<WaterModel> loadedWaterData = [];
+    extractedData.forEach((waterId, waterData) {
+      loadedWaterData.add(
+        WaterModel(
+          id: waterId,
+          amount: waterData['amount'],
+          dateTime: DateTime.parse(waterData['date']),
+          unit: waterData['unit'],
+        ),
+      );
+    });
+    return loadedWaterData;
   }
 }
