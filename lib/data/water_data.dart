3b87d5a0653d6328a1fc3c4782cd5ba7d22ddfb4
Aniket-Vyass <aniketvyas05@gmail.com>
2026-01-31 23:45:17 +0530

    data display 1

--- lib/data/water_data.dart
+++ lib/data/water_data.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:water_intake_tracking_app_firebase/models/water_model.dart';
 
-class WaterData extends ChangeNotifier {
+class WaterData extends ChangeNotifier{
   List<WaterModel> waterDataList = [];
-
-  //
 }
