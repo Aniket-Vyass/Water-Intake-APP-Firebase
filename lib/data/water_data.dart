22d0cc2632376bcbaf78e0e6bc28f81c6f299825
Aniket-Vyass <aniketvyas05@gmail.com>
2026-01-31 23:22:42 +0530

    bar data display

---
+++ lib/data/water_data.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:water_intake_tracking_app_firebase/models/water_model.dart';
+
+class WaterData extends ChangeNotifier {
+  List<WaterModel> waterDataList = [];
+
+  //
+}
